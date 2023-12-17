b6236aa1febc0af287d1bba27797feeb0064fbe9
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-17 15:42:27 -0300

    feat: created CreditService interface into services layer

---
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/services/CreditService.kt
@@ -0,0 +1,10 @@
+package br.com.diobootcamp.credit.application.system.services
+
+import br.com.diobootcamp.credit.application.system.entities.Credit
+import java.util.UUID
+
+interface CreditService {
+    fun save(credit: Credit): Credit;
+    fun findAllByCustomer(customerId: Long): List<Credit>;
+    fun findByCreditCode(creditCode: UUID): Credit;
+}
