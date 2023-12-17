bbb1106d64b8127a976f80c046703419eb089676
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-17 10:13:02 -0300

    feat: create ORM entities with Hibernate

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/entities/Credit.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/entities/Credit.kt
@@ -1,16 +1,19 @@
 package br.com.diobootcamp.credit.application.system.entities
 
 import br.com.diobootcamp.credit.application.system.entities.enummeration.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
+@Table(name = "credit")
 data class Credit(
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallment: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null,
-    val id: Long? = null
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallment: Int = 0,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
