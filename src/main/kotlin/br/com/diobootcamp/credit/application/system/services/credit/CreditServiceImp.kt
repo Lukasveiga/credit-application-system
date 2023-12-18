0c182a067b463be091af60fd098bb2499b3c5a3e
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-17 22:57:21 -0300

    feat: created CreditServiceImp

---
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/services/credit/CreditServiceImp.kt
@@ -0,0 +1,28 @@
+package br.com.diobootcamp.credit.application.system.services.credit
+
+import br.com.diobootcamp.credit.application.system.entities.Credit
+import br.com.diobootcamp.credit.application.system.repositories.CreditRepository
+import br.com.diobootcamp.credit.application.system.services.customer.CustomerService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditServiceImp(private val creditRepository: CreditRepository, private val customerService: CustomerService): CreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> {
+        return this.creditRepository.findAllByCustomerId(customerId)
+    }
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit = this.creditRepository.findByCreditCode(creditCode) ?: throw RuntimeException("Credit code $creditCode not found")
+
+        return if (credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
+    }
+}
