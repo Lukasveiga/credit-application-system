41175e08fce2eabc74222a4e920fd9dec3683418
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-17 22:56:29 -0300

    feat: add two more jpa functions for CreditRepository

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/repositories/CreditRepository.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/repositories/CreditRepository.kt
@@ -2,5 +2,13 @@ package br.com.diobootcamp.credit.application.system.repositories
 
 import br.com.diobootcamp.credit.application.system.entities.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.UUID
 
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long> {
+
+    fun findByCreditCode(creditCode: UUID): Credit?
+
+    @Query(value = "SELECT * FROM credit WHERE customer_id = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
+}
