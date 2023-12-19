1e7c9a5e1fa1ea6a245a12caea8ac204bb38521f
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-19 11:24:01 -0300

    feat: Created update method into CustomerService

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/services/customer/CustomerService.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/services/customer/CustomerService.kt
@@ -1,9 +1,12 @@
 package br.com.diobootcamp.credit.application.system.services.customer
 
+import br.com.diobootcamp.credit.application.system.dto.CustomerUpdateDTO
 import br.com.diobootcamp.credit.application.system.entities.Customer
 
 interface CustomerService {
-    fun save(customer: Customer): Customer;
-    fun findById(customerId: Long): Customer;
-    fun delete(customerId: Long): Unit;
+    fun save(customer: Customer): Customer
+    fun findById(customerId: Long): Customer
+
+    fun update(customerId: Long, customerUpdateDTO: CustomerUpdateDTO): Customer
+    fun delete(customerId: Long): Unit
 }
--- src/main/kotlin/br/com/diobootcamp/credit/application/system/services/customer/CustomerServiceImp.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/services/customer/CustomerServiceImp.kt
@@ -1,5 +1,6 @@
 package br.com.diobootcamp.credit.application.system.services.customer
 
+import br.com.diobootcamp.credit.application.system.dto.CustomerUpdateDTO
 import br.com.diobootcamp.credit.application.system.entities.Customer
 import br.com.diobootcamp.credit.application.system.repositories.CustomerRepository
 import org.springframework.stereotype.Service
@@ -12,6 +13,14 @@ class CustomerServiceImp(private val customerRepository: CustomerRepository): Cu
     override fun findById(customerId: Long): Customer =
         this.customerRepository.findById(customerId)
             .orElseThrow { throw RuntimeException("Id $customerId not found") }
+
+    override fun update(customerId: Long, customerUpdateDTO: CustomerUpdateDTO): Customer {
+        val customer = this.findById(customerId)
+        val updatedCustomer = customerUpdateDTO.toEntity(customer)
+        this.customerRepository.save(updatedCustomer)
+        return updatedCustomer
+    }
+
 
     override fun delete(customerId: Long) {
         this.findById(customerId)
