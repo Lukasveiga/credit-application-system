b4d1e9a3909e4af2d7ab6f5f0c89d68c3739d914
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-18 22:56:21 -0300

    feat: Created CustomerDTO

---
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/dto/CustomerDTO.kt
@@ -0,0 +1,32 @@
+package br.com.diobootcamp.credit.application.system.dto
+
+import br.com.diobootcamp.credit.application.system.entities.Address
+import br.com.diobootcamp.credit.application.system.entities.Customer
+import java.math.BigDecimal
+
+data class CustomerDTO(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String
+) {
+
+    fun toEntity(): Customer = Customer(
+        firstName = this.firstName,
+        lastName = this.lastName,
+        cpf = this.cpf,
+        income = this.income,
+        email = this.email,
+        password = this.password,
+        address = Address(
+            zipCode = this.zipCode,
+            street = this.street
+        )
+
+    )
+
+}
