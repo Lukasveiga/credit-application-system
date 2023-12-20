749f1963977b51c2c833e22431691bf132d75d21
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-19 23:28:33 -0300

    refactor: Correct param name of CustomerUpdateDTO DTO

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/dto/customer/CustomerUpdateDTO.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/dto/customer/CustomerUpdateDTO.kt
@@ -4,14 +4,14 @@ import br.com.diobootcamp.credit.application.system.entities.Customer
 import java.math.BigDecimal
 
 data class CustomerUpdateDTO(
-    val fristName: String,
+    val firstName: String,
     val lastName: String,
     val income: BigDecimal,
     val zipCode: String,
     val street: String
 ) {
     fun toEntity(customer: Customer): Customer {
-        customer.firstName = this.fristName
+        customer.firstName = this.firstName
         customer.lastName = this.lastName
         customer.income = this.income
         customer.address.zipCode = this.zipCode
