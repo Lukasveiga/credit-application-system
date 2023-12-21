406876f40897305d3e8372764244c4f1353dd20c
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-20 22:56:58 -0300

    feat: Created handler for DataAccessException and BusinessExeptions

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/exception/RestExceptionHandler.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/exception/RestExceptionHandler.kt
@@ -1,5 +1,7 @@
 package br.com.diobootcamp.credit.application.system.exception
 
+import br.com.diobootcamp.credit.application.system.services.exceptions.BusinessExcetion
+import org.springframework.dao.DataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
@@ -28,6 +30,32 @@ class RestExceptionHandler {
                 satus = HttpStatus.BAD_REQUEST.value(),
                 exception = ex.javaClass.simpleName.toString(),
                 details = errors
+            ), HttpStatus.BAD_REQUEST
+        )
+    }
+
+    @ExceptionHandler(DataAccessException::class)
+    fun handlerDataAccessExceptions(ex: DataAccessException): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity(
+            ExceptionDetails(
+                title = "Bad Request. Consult documentation",
+                timestamp = LocalDateTime.now(),
+                satus = HttpStatus.CONFLICT.value(),
+                exception = ex.javaClass.simpleName.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message.toString())
+            ), HttpStatus.CONFLICT
+        )
+    }
+
+    @ExceptionHandler(BusinessExcetion::class)
+    fun handlerBusinessExceptions(ex: BusinessExcetion): ResponseEntity<ExceptionDetails> {
+        return ResponseEntity(
+            ExceptionDetails(
+                title = "Bad Request. Consult documentation",
+                timestamp = LocalDateTime.now(),
+                satus = HttpStatus.BAD_REQUEST.value(),
+                exception = ex.javaClass.simpleName.toString(),
+                details = mutableMapOf(ex.cause.toString() to ex.message.toString())
             ), HttpStatus.BAD_REQUEST
         )
     }
