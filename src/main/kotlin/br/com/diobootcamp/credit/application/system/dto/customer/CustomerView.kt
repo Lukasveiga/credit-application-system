758f5b7df92e86d33206f3d2aa6e15106835e717
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-26 10:13:11 -0300

    refactor: Insert id param to CustomerView DTO

--- src/main/kotlin/br/com/diobootcamp/credit/application/system/dto/customer/CustomerView.kt
+++ src/main/kotlin/br/com/diobootcamp/credit/application/system/dto/customer/CustomerView.kt
@@ -4,21 +4,23 @@ import br.com.diobootcamp.credit.application.system.entities.Customer
 import java.math.BigDecimal
 
 data class CustomerView(
-    val fistName: String,
+    val firstName: String,
     val lastName: String,
     val cpf: String,
     val income: BigDecimal,
     val email: String,
     val zipCode: String,
-    val street: String
+    val street: String,
+    val id: Long?
 ) {
     constructor(customer: Customer): this (
-        fistName = customer.firstName,
+        firstName = customer.firstName,
         lastName = customer.lastName,
         cpf = customer.cpf,
         income = customer.income,
         email = customer.email,
         zipCode = customer.address.zipCode,
-        street = customer.address.street
+        street = customer.address.street,
+        id = customer.id
     )
 }
