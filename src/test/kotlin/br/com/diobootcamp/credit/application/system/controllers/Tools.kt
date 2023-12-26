f4d78efcdff618351dc9d828e9482ba133a2af4e
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-26 10:14:35 -0300

    feat: Created Tools class to CustomerControllerIT integration test

---
+++ src/test/kotlin/br/com/diobootcamp/credit/application/system/controllers/Tools.kt
@@ -0,0 +1,30 @@
+package br.com.diobootcamp.credit.application.system.controllers
+
+import br.com.diobootcamp.credit.application.system.dto.customer.CustomerDTO
+import java.math.BigDecimal
+
+internal class Tools {
+
+    companion object {
+
+        fun buildCustomerDTO(
+            firstName: String = "Lukas",
+            lastName: String = "Veiga",
+            cpf: String = "883.799.550-48",
+            email: String = "test@email.com",
+            password: String = "testPassword",
+            zipCode: String = "13545",
+            street: String = "Rua Dois Terços",
+            income: BigDecimal = BigDecimal(0)
+        ): CustomerDTO = CustomerDTO(
+            firstName = firstName,
+            lastName = lastName,
+            cpf = cpf,
+            email = email,
+            password = password,
+            zipCode = zipCode,
+            street = street,
+            income = income,
+        )
+    }
+}
