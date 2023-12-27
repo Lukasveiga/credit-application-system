6be70f8ceb6a4827bb1e059c39f86174de815e72
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-27 10:20:44 -0300

    feat: Created Customer Integration test for shouldNotUpdateCustomerAndReturn400StatusCode case

--- src/test/kotlin/br/com/diobootcamp/credit/application/system/controllers/customer/CustomerControllerIT.kt
+++ src/test/kotlin/br/com/diobootcamp/credit/application/system/controllers/customer/CustomerControllerIT.kt
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import kotlin.random.Random
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -189,4 +190,24 @@ class CustomerControllerIT {
             .andExpect(MockMvcResultMatchers.jsonPath("$.income").value(customerUpdateDTO.income))
             .andDo(MockMvcResultHandlers.print())
     }
+
+    @Test
+    fun shouldNotUpdateCustomerAndReturn400StatusCode() {
+        // given
+        val invalidId: Long = Random.nextLong()
+        val customerUpdateDTO: CustomerUpdateDTO = Tools.builderCustomerUpdateDTO()
+        val customerUpdateDTOAsString: String = objectMapper.writeValueAsString(customerUpdateDTO)
+        // when - then
+        mockMvc.perform(MockMvcRequestBuilders.patch("$URL?customerId=$invalidId")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(customerUpdateDTOAsString))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request. Consult documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception").value("BusinessExcetion"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details.null").value("Id $invalidId not found"))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
 }
