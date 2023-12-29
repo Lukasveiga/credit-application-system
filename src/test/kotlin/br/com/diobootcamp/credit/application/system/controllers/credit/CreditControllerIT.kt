6d6d5f6e6f8d2e8313ad9a3fb53067d75b72d276
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-29 14:40:22 -0300

    feat: Created Credit Integration test shouldCreateCreditAndReturn201StatusCode case

--- src/test/kotlin/br/com/diobootcamp/credit/application/system/controllers/credit/CreditControllerIT.kt
+++ src/test/kotlin/br/com/diobootcamp/credit/application/system/controllers/credit/CreditControllerIT.kt
@@ -1,6 +1,73 @@
 package br.com.diobootcamp.credit.application.system.controllers.credit
 
 import br.com.diobootcamp.credit.application.system.controllers.IntegrationTestConfig
+import br.com.diobootcamp.credit.application.system.controllers.customer.CustomerControllerIT
+import br.com.diobootcamp.credit.application.system.dto.credit.CreditDTO
+import br.com.diobootcamp.credit.application.system.entities.Customer
+import br.com.diobootcamp.credit.application.system.repositories.CreditRepository
+import br.com.diobootcamp.credit.application.system.repositories.CustomerRepository
+import br.com.diobootcamp.credit.application.system.tools.CreditTools
+import br.com.diobootcamp.credit.application.system.tools.CustomerTools
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class CreditControllerIT: IntegrationTestConfig() {
+
+    @Autowired
+    private lateinit var creditRepository: CreditRepository
+
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/v1/credits"
+        val customerTest: Customer = CustomerTools.builderCustomer(id = null)
+    }
+
+    @BeforeEach
+    fun setUp() {
+        creditRepository.deleteAll()
+        customerRepository.deleteAll()
+        customerRepository.save(customerTest)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        creditRepository.deleteAll()
+        customerRepository.deleteAll()
+    }
+
+    @Test
+    fun shouldCreateCreditAndReturn201StatusCode() {
+        // given
+        val creditDTO: CreditDTO = CreditTools.builderCreditDTO(customerId = 1)
+        val creditDTOAsString: String = objectMapper.writeValueAsString(creditDTO)
+        // when
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(creditDTOAsString))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.creditCode").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.creditValue").value(creditDTO.creditValue))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfInstallments").value(creditDTO.numberOfInstallments))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.emailCustomer").value(customerTest.email))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.incomeCustomer").value(customerTest.income.longValueExact()))
+            .andDo(MockMvcResultHandlers.print())
+    }
 }
