aa39b98c104e068aa68f42fd37f37a8e0aecb158
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-29 14:38:47 -0300

    feat: Created builderCreditDTO tool function to Credit Integration test

--- src/test/kotlin/br/com/diobootcamp/credit/application/system/tools/CreditTools.kt
+++ src/test/kotlin/br/com/diobootcamp/credit/application/system/tools/CreditTools.kt
@@ -1,5 +1,6 @@
 package br.com.diobootcamp.credit.application.system.tools
 
+import br.com.diobootcamp.credit.application.system.dto.credit.CreditDTO
 import br.com.diobootcamp.credit.application.system.entities.Credit
 import br.com.diobootcamp.credit.application.system.entities.Customer
 import java.math.BigDecimal
@@ -11,7 +12,7 @@ internal class CreditTools {
     companion object {
         fun builderCredit(
             creditValue: BigDecimal = BigDecimal.valueOf(500.0),
-            dayFirstInstallment: LocalDate = LocalDate.of(2023, Month.DECEMBER, 26),
+            dayFirstInstallment: LocalDate = LocalDate.now().plusDays(1),
             numberOfInstallments: Int = 5,
             customer: Customer
         ): Credit = Credit(
@@ -19,6 +20,18 @@ internal class CreditTools {
             dayFirstInstallment = dayFirstInstallment,
             numberOfInstallment = numberOfInstallments,
             customer = customer
+        )
+
+        fun builderCreditDTO(
+            creditValue: BigDecimal = BigDecimal.valueOf(500.0),
+            dayFirstInstallment: LocalDate = LocalDate.now().plusDays(1),
+            numberOfInstallments: Int = 5,
+            customerId: Long
+        ): CreditDTO = CreditDTO(
+            creditValue = creditValue,
+            dayFirstInstallment = dayFirstInstallment,
+            numberOfInstallments = numberOfInstallments,
+            customerId = customerId
         )
     }
 
